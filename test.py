e764d31c692b50b3f53b6fe1b110363f4939ca34
viru027 <131556028+viru027@users.noreply.github.com>
2026-07-21 13:48:01 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-from config import GEMINI_API_KEY
-
-genai.configure(api_key=GEMINI_API_KEY)
-
-for model in genai.list_models():
-    if "generateContent" in model.supported_generation_methods:
-        print(model.name)
